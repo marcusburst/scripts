9e067e371f9364be81f08b0cca29d86b465ef371
Marcus Burst <marcus.burst@gmail.com>
2019-09-20 09:38:51 +1000

    Update LogOffDisconnectedUsers.ps1

--- Powershell/LogOffDisconnectedUsers.ps1
+++ Powershell/LogOffDisconnectedUsers.ps1
@@ -1,1 +1,4 @@
-﻿quser | Select-String "Disc" | ForEach {logoff ($_.tostring() -split ' +')[2]}
+### Logs off users who have a disconnected user session state
+### Originally created for meeting room PCs to circumvent issue with Box Drive not working if someone else is logged in at the same time
+
+quser | Select-String "Disc" | ForEach {logoff ($_.tostring() -split ' +')[2]}
