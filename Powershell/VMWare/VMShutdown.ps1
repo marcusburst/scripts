5bd5ac8ec4f18151a9d4a088e8ca89ae0696bc1d
Marcus Burst <marcus.burst@gmail.com>
2019-09-20 13:19:56 +1000

    Adding more scripts to repo

---
+++ Powershell/VMWare/VMShutdown.ps1
@@ -0,0 +1,19 @@
+﻿## Imports a list of VM's from a .txt file and shuts them down ##
+
+foreach($vmName in (Get-Content -Path .\vmnames.txt)){
+
+    $vm = Get-VM -Name $vmName
+
+    if($vm.Guest.State -eq "Running"){
+
+        Shutdown-VMGuest -VM $vm -Confirm:$false
+
+    }
+
+    else{
+
+        Stop-VM -VM $vm -Confirm:$false
+
+    }
+
+}
