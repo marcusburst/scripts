db237b7cdfc5b4624e597ffe9d25252554b1f24d
Marcus Burst <marcus.burst@gmail.com>
2019-09-20 13:42:50 +1000

    Adding Scripts to Repo

---
+++ Powershell/Windows/StartSlideshow.ps1
@@ -0,0 +1,9 @@
+﻿## Auto-starts a powerpoint file in slideshow mode, great for public/display TV's. Ideally you want to call this script with a .bat file in the startup folder of the host ##
+
+$ppt = "\\trading.imc.intra\apac\UserProfiles\apactv\Documents\Slideshow\Please update this version.pptx"
+
+$app = New-Object -ComObject powerpoint.application
+          $pres = $app.Presentations.open($ppt)
+          $app.visible = "msoTrue"
+          $pres.SlideShowSettings.Run()                         
+          $pres.visible = "msoTrue"
