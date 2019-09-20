36e96d1ce46c2ed41038fa40126cd6ef98dc0a9e
Marcus Burst <marcus.burst@gmail.com>
2019-09-20 13:08:28 +1000

    Adding scripts to Repo

---
+++ Powershell/Active Directory/ForceChangeDomainController.ps1
@@ -0,0 +1,5 @@
+﻿## Forces a PC to talk to a specific DC ##
+## Make sure you change the values of the client computer name and the DC you want to switch to ##
+## Works best if run on the machine locally ##
+
+nltest /SERVER:HOSTNAMEOFPC /SC_RESET:DOMAIN\DOMAINCONTROLLER
