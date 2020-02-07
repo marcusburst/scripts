cc9bab2172ac6c6344de292411dfe30a25b931de
Marcus Burst <mburst@trading.imc.intra>
2020-02-07 16:58:24 +1100

    New scripts

---
+++ Powershell/Windows/CreateLocalAdminUser.ps1
@@ -0,0 +1,25 @@
+$Account = 'apacsupport'
+$Password = Read-Host "Enter Password" -AsSecureString
+$Administrators = 'Administrators'
+$PCList = Get-Content -Path 'C:\temp\InfraServers.txt'
+
+foreach($PC in $PCList){
+
+$UserExists = [bool](Get-LocalUser -Name $Account -ErrorAction SilentlyContinue)
+
+if(-not ($UserExists)){
+
+    New-LocalUser -Name $Account -Password $Password -PasswordNeverExpires
+    Write-Host "Created user $Account"
+    Add-LocalGroupMember -Group $Administrators -Member $Account
+    Write-Host "Added User $Account to $Administrators group"
+
+}
+
+else{
+
+    Write-Host "$Account already exists"
+    exit
+
+}
+}
