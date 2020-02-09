353375f19e5a408b8c3b758a9bfa10becc206562
Marcus Burst <mburst@trading.imc.intra>
2020-02-10 09:02:12 +1100

    Fixing script

--- Powershell/Windows/CreateLocalAdminUser.ps1
+++ Powershell/Windows/CreateLocalAdminUser.ps1
@@ -5,44 +5,44 @@ $ComputerNames = Get-Content 'C:\Windows\Temp\InfraServers.txt'
 
 Invoke-Command -ComputerName $ComputerNames -ScriptBlock {
 
-    if ($PSVersionTable.PSVersion.Major -ge 5 -and $PSVersionTable.PSVersion.Minor -ge 1) {
+if($PSVersionTable.PSVersion.Major -ge 5 -and $PSVersionTable.PSVersion.Minor -ge 1){
 
-        $UserExists = [bool](Get-LocalUser -Name $Using:Account)
+$UserExists = [bool](Get-LocalUser -Name $Using:Account)
 
-        if (-not ($UserExists)) {
+if(-not ($UserExists)){
 
-            New-LocalUser -Name $Using:Account -Password $Using:Password -PasswordNeverExpires
-            Write-Host "Created user $Using:Account on $env:COMPUTERNAME"
-            Add-LocalGroupMember -Group $Using:Administrators -Member $Using:Account
-            Write-Host "Added User $Using:Account to $Using:Administrators group on $env:COMPUTERNAME"
+    New-LocalUser -Name $Using:Account -Password $Using:Password -PasswordNeverExpires
+    Write-Host "Created user $Using:Account on $env:COMPUTERNAME"
+    Add-LocalGroupMember -Group $Using:Administrators -Member $Using:Account
+    Write-Host "Added User $Using:Account to $Using:Administrators group on $env:COMPUTERNAME"
 
-        }
+}
 
-        else {
+else{
 
-            Write-Host "$Using:Account already exists on $env:COMPUTERNAME"
-            exit
-        }
+    Write-Host "$Using:Account already exists on $env:COMPUTERNAME"
+    exit
+}
+}
 
-        else {
+else{
 
-            $ComputerName = $env:ComputerName
-            $UserExistsCIM = [bool](Get-WMIObject -ClassName Win32_UserAccount -Computername $ComputerName | Where-Object Name -eq '$Using:Account')
+    $ComputerName = $env:ComputerName
+    $UserExistsCIM = [bool](Get-WMIObject -ClassName Win32_UserAccount -Computername $ComputerName | Where-Object Name -eq '$Using:Account')
 
-            if (-not ($UserExistsCIM)) {
+    if(-not ($UserExistsCIM)){
 
-                net user /add $Using:Account 1mc$ecret
-                Write-Host "Created user $Using:Account on $env:COMPUTERNAME"
-                net localgroup $Using:Administrators $Using:Account /add
-                Write-Host "Added User $Using:Account to $Using:Administrators group on $env:COMPUTERNAME"
+    net user /add $Using:Account 1mc$ecret
+    Write-Host "Created user $Using:Account on $env:COMPUTERNAME"
+    net localgroup $Using:Administrators $Using:Account /add
+    Write-Host "Added User $Using:Account to $Using:Administrators group on $env:COMPUTERNAME"
 
-            }
-
-            else {
-                Write-Host "$Using:Account already existson $env:COMPUTERNAME"
-                exit
-
-            }
-        }
     }
+
+else{
+    Write-Host "$Using:Account already existson $env:COMPUTERNAME"
+    exit
+
+}
+}
 }
