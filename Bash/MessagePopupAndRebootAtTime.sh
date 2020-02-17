f53dfdfdb938a9d7bf5e23dc9804426a45472612
Marcus Burst <mburst@trading.imc.intra>
2020-02-17 13:48:42 +1100

    Adding script

---
+++ Bash/MessagePopupAndRebootAtTime.sh
@@ -0,0 +1,1 @@
+nts" to display dialog "Your laptop is in violation of a compliance policy and has been scheduled to reboot at 10pm tonight to install critical security updates. - Please talk to the IT Support Team if you have any issues."'
